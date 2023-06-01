91eb40fdb00389f25084d06fe59d7b10a1734a7c
Benjamin Yong <benj_yongzh@hotmail.com>
2023-06-01 16:53:16 +0800

    Learn about basic REST methods and how to invoke them in cURL.

--- app.js
+++ app.js
@@ -1,13 +1,71 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+let users = {
+  1: {
+    id: "1",
+    username: "Robin Wieruch",
+  },
+  2: {
+    id: "2",
+    username: "Dave Davids",
+  },
+};
+
+let messages = {
+  1: {
+    id: "1",
+    text: "Hello World",
+    userId: "1",
+  },
+  2: {
+    id: "2",
+    text: "By World",
+    userId: "2",
+  },
+};
+
+app.get("/users", (req, res) => {
+  return res.send(Object.values(users));
 });
+
+app.get("/users/:id", (req, res) => {
+  return res.send(Object.values(users[req.params.id]));
+});
+
+app.get("/messages", (req, res) => {
+  return res.send(Object.values(messages));
+});
+
+app.get("/messages/:id", (req, res) => {
+  return res.send(Object.values(messages[req.params.id]));
+});
+
+app.post("/messages", (req, res) => {
+  const id = uuidv4();
+  const message = { id };
+  messages[id] = message;
+  return res.send(message);
+});
+
+// app.post("/users", (req, res) => {
+//   return res.send("POST http method on a user resource.");
+// });
+
+// app.put("/users/:id", (req, res) => {
+//   return res.send(`PUT http method on a user resource, ID: ${req.params.id}.`);
+// });
+
+// app.delete("/users/:id", (req, res) => {
+//   return res.send(
+//     `DELETE http method on a user resource, ID: ${req.params.id}.`
+//   );
+// });
 
 app.listen(process.env.PORT || 3000, () =>
   console.log("App listening to port 3000")
