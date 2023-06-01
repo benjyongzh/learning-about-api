9da6c1f1e3f1fec4803491a2f55588b3f35fdfcf
Benjamin Yong <benj_yongzh@hotmail.com>
2023-06-01 18:51:31 +0800

    Learn makeshift auth and DELETE REST.

--- app.js
+++ app.js
@@ -2,54 +2,48 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const { v4: uuidv4 } = require("uuid");
+const models = require("./models/index");
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-let users = {
-  1: {
-    id: "1",
-    username: "Robin Wieruch",
-  },
-  2: {
-    id: "2",
-    username: "Dave Davids",
-  },
-};
+app.use((req, res, next) => {
+  req.me = models.users[1];
+  next();
+});
 
-let messages = {
-  1: {
-    id: "1",
-    text: "Hello World",
-    userId: "1",
-  },
-  2: {
-    id: "2",
-    text: "By World",
-    userId: "2",
-  },
-};
+app.get("/session", (req, res) => {
+  res.send(models.users[req.me.id]);
+});
 
 app.get("/users", (req, res) => {
-  return res.send(Object.values(users));
+  return res.send(Object.values(models.users));
 });
 
 app.get("/users/:id", (req, res) => {
-  return res.send(Object.values(users[req.params.id]));
+  return res.send(Object.values(models.users[req.params.id]));
 });
 
 app.get("/messages", (req, res) => {
-  return res.send(Object.values(messages));
+  return res.send(Object.values(models.messages));
 });
 
 app.get("/messages/:id", (req, res) => {
-  return res.send(Object.values(messages[req.params.id]));
+  return res.send(Object.values(models.messages[req.params.id]));
 });
 
 app.post("/messages", (req, res) => {
   const id = uuidv4();
-  const message = { id };
-  messages[id] = message;
+  const message = { id, text: req.body.text, userId: req.me.id };
+  models.messages[id] = message;
+  return res.send(message);
+});
+
+app.delete("/messages/:id", (req, res) => {
+  const { [req.params.id]: message, ...otherMessages } = models.messages;
+  models.messages = otherMessages;
   return res.send(message);
 });
 
